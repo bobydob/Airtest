ccdaf7d394f209f3b4c19f90a720486200c87b95
bobydob <80217817+bobydob@users.noreply.github.com>
2024-05-01 07:19:26 +0300

    Add files via upload

---
+++ 5633/unityUrls.js
@@ -0,0 +1,9 @@
+window.loadedUrls = true;
+
+function getBuildUrl() {
+	return "https://justplay-cdn.playtika.com/justbuild/NewCI/1v1/Prod/346/2de6133b676344f28ecc2a1fe7869573";
+}
+
+function getGameLoaderUrl() {
+	return getBuildUrl() + "/WebGL.loader.js";
+}
