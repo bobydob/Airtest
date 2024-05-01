cca29fbd51f1b6da709112aeda247ef565f2ad7c
bobydob <80217817+bobydob@users.noreply.github.com>
2024-05-01 10:30:10 +0300

    Delete 5633 directory

--- 5633/unityUrls.js
+++
@@ -1,9 +0,0 @@
-window.loadedUrls = true;
-
-function getBuildUrl() {
-	return "https://justbuild.nyc3.cdn.digitaloceanspaces.com/NewCI/1v1/Prod/346/2de6133b676344f28ecc2a1fe7869573";
-}
-
-function getGameLoaderUrl() {
-	return getBuildUrl() + "/WebGL.loader.js";
-}
