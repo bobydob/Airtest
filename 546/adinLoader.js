573fc7b6467d9c48f11a1920720f8f81df99b2a7
bobydob <80217817+bobydob@users.noreply.github.com>
2024-05-19 07:47:13 +0300

    Update adinLoader.js

--- 546/adinLoader.js
+++ 546/adinLoader.js
@@ -1,1 +1,1 @@
-const getAdinDomain = 'https://cdn.jsdelivr.net/gh/bobydob/Airtest@8f36080ba925e8afbbeaff122140b2f0cc0b537e/546/adinGameLoader.js';
+const getAdinDomain = 'https://cdn.jsdelivr.net/gh/bobydob/Airtest@33dfcb33275ca7364a3db61776a6292f263c5216/546/adinGameLoader.js';
